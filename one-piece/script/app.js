0b2cf99687db25ec1cbcff7aec9185eb45409b5a
luizottavio <102751708+luizottavio@users.noreply.github.com>
2023-09-11 16:22:08 -0300

    .

---
+++ one-piece/script/app.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('.form')
+const result = document.querySelector('.result')
+const correctAnswers = ['C','D','A','C']
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    
+    let score = 0
+    const userAnswers = [
+        form.inputOpition1.value,
+        form.inputOpition2.value,
+        form.inputOpition3.value,
+        form.inputOpition4.value
+    ]
+
+    userAnswers.forEach((userAnswer, index) => {
+        if(userAnswer === correctAnswers[index]) {
+            score+=25
+        }
+    })
+
+    let couter = 0
+
+    const timer = setInterval(() => {
+        if (couter === score){
+            clearInterval(timer)
+        }
+        result.textContent = `${couter}%`
+        couter++
+    }, 50)
+})
